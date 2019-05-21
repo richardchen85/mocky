0060101001cff08f311c60d15a933a3125ccc524
陈礼强 <cdchenliqiang@jd.com>
2019-05-21 10:10:53 +0800

    local build

---
+++ mocky-fe/build/precache-manifest.242c0570f4d5eb68c6f7a7cd5f17e0bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1f174ad150687008faf9",
+    "url": "/static/js/main.0cb1c08f.chunk.js"
+  },
+  {
+    "revision": "0a229ac3ccce28ccc441",
+    "url": "/static/js/2.6ed3475b.chunk.js"
+  },
+  {
+    "revision": "1f174ad150687008faf9",
+    "url": "/static/css/main.c85c2f75.chunk.css"
+  },
+  {
+    "revision": "0a229ac3ccce28ccc441",
+    "url": "/static/css/2.814230e7.chunk.css"
+  },
+  {
+    "revision": "60181be966e6368c3c767d6fb4d7ea16",
+    "url": "/index.html"
+  }
+];
