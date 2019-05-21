0b0d44fa25bbe80f8f752d4a8ef27c41a49971ee
陈礼强 <cdchenliqiang@jd.com>
2019-05-21 09:54:27 +0800

    do not use misc.chenliqiang.cn

--- mocky-fe/copy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-version=$(cat package.json | grep -oE "\"version\": \"([0-9.]+)\"" | grep -oE "[0-9.]+")
-static=../../misc-clq/mocky/${version}
-cp -f ./build/index.html ./deploy/
-rm -rf ${static}
-mkdir -p ${static}/static
-cp -R -f ./build/static/ ${static}/static
